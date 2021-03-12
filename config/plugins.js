c45ead297c3d19d828b634ab20d7c1bbe6e61715
Hugo V <hugopcveloso@gmail.com>
2021-03-12 17:22:33 +0000

    fixing cloudinary upload

--- config/plugins.js
+++ config/plugins.js
@@ -1,16 +1,27 @@
 if (process.env.NODE_ENV === "production") {
-  module.exports = {
-    provider: "cloudinary",
-    providerOptions: {
-      cloud_name: process.env.PROVIDER_CLOUD_NAME,
-      api_key: process.env.PROVIDER_API_KEY,
-      api_secret: process.env.PROVIDER_API_SECRET,
+  module.exports = ({ env }) => ({
+    upload: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: process.env.PROVIDER_CLOUD_NAME,
+        api_key: process.env.PROVIDER_API_KEY,
+        api_secret: process.env.PROVIDER_API_SECRET,
+      },
     },
-    actionOptions: {
-      upload: {},
-      delete: {},
-    },
-  };
+  });
+
+  // module.exports = {
+  //   provider: "cloudinary",
+  //   providerOptions: {
+  //     cloud_name: process.env.PROVIDER_CLOUD_NAME,
+  //     api_key: process.env.PROVIDER_API_KEY,
+  //     api_secret: process.env.PROVIDER_API_SECRET,
+  //   },
+  //   actionOptions: {
+  //     upload: {},
+  //     delete: {},
+  //   },
+  // };
 } else {
   // to use the default local provider you can return an empty configuration
   module.exports = {};
