8928ea359d53afd0570887de3bef12a8b34d05b4
Hugo V <hugopcveloso@gmail.com>
2021-03-12 16:29:07 +0000

    fixing deployment issues

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,14 @@
 if (process.env.NODE_ENV === "production") {
   module.exports = {
-    provider: "providerName",
+    provider: "cloudinary",
     providerOptions: {
       cloud_name: process.env.PROVIDER_CLOUD_NAME,
       api_key: process.env.PROVIDER_API_KEY,
       api_secret: process.env.PROVIDER_API_SECRET,
+    },
+    actionOptions: {
+      upload: {},
+      delete: {},
     },
   };
 } else {
